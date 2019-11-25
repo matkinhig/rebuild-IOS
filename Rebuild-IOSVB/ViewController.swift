c6d9593574c4f3870d7146313445949f70fa0e10
HoaLT <letienhoa@vietbank.com.vn>
2019-11-25 11:02:42 +0700

    Initial Commit

---
+++ Rebuild-IOSVB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rebuild-IOSVB
+//
+//  Created by Mắt Kính IG on 11/25/19.
+//  Copyright © 2019 Mắt Kính IG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
